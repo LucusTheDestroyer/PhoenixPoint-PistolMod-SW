eded97c95731b5c528a28e03d152dda1e9668712
Luc <lucoejames@hotmail.co.uk>
2026-06-24 15:31:12 +0100

    Split Sniper/Berserker options in config

--- PistolProficiencyMod/PistolProficiencyModConfig.cs
+++ PistolProficiencyMod/PistolProficiencyModConfig.cs
@@ -8,7 +8,10 @@ namespace PistolProficiencyMod
 	/// Config is serialized on disk as json.
 	public class PistolProficiencyModConfig : ModConfig
 	{		
-		[ConfigField(text: "% Bonus Proficiency", description: "Adjust the bonus percentage Pistol accuracy/damage given to the Sniper/Berserker (Default: 20%)")]
-		public int Compensation = 20; // 20% is the default multiplier
+		[ConfigField(text: "Sniper Accuracy Bonus", description: "Adjusts the bonus percentage Handgun accuracy given to the Sniper (Default: 20%)")]
+		public int SniperBonus = 20; // 20% is the default multiplier
+
+		[ConfigField(text: "Berserker Damage Bonus", description: "Adjusts the bonus percentage Handgun damage given to the Berserker (Default: 20%)")]
+		public int BerserkerBonus = 20; // 20% is the default multiplier
 	}
 }
